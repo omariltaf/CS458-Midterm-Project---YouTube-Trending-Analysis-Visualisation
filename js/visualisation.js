abd0cfeb04483fb7b7a38eec5c51a726b8b17a15
Omar <omariltaf@hotmail.co.uk>
2018-06-14 20:26:56 -0700

    Added wordcloud functionality.

--- js/visualisation.js
+++ js/visualisation.js
@@ -1,8 +1,10 @@
 var currentChart;
 var ctx;
+var wordcloud;
 
 window.onload = function() {
     ctx = document.getElementById("currentChart");
+    wordcloud = document.getElementById("wordcloud");
     currentChart = drawPublishTimeByViewsChart(ctx);
 }
 
@@ -16,15 +18,6 @@ window.onload = function() {
 //         console.log(JSON_data);
 //     }
 // }
-
-// Dropdown functionality
-document.addEventListener("DOMContentLoaded", function() {
-    var elems = document.querySelectorAll(".dropdown-trigger");
-    var options = {
-        coverTrigger: false
-    };
-    var instances = M.Dropdown.init(elems, options);
-});
 
 // Chart.js functionality
 function addData(chart, label, data) {
@@ -43,6 +36,23 @@ function removeData(chart) {
     chart.update();
 }
 
+// Word Cloud functionality
+function drawWordCloud() {
+  d3.wordcloud()
+    .size([800, 380])
+    .selector('#wordcloud')
+    .words([
+      {text: 'a', size: 5},
+      {text: 'b', size: 5},
+      {text: 'c', size: 5},
+      {text: 'd', size: 5},
+      {text: 'e', size: 5},
+      {text: 'f', size: 5},
+      {text: 'g', size: 5},
+    ])
+    .start();
+}
+
 // jQuery
 $(document).ready(function() {
     // Detects when to load a new visualisation
@@ -50,36 +60,34 @@ $(document).ready(function() {
         switch ($("input[name=factor]:checked", "#change_factor_form").val()) {
             case "Publish Time by Average Views":
                 currentChart.destroy();
+                $("#wordcloud").empty();
                 currentChart = drawPublishTimeByViewsChart(ctx);
                 break;
             case "Publish Time by Video Count":
                 currentChart.destroy();
+                $("#wordcloud").empty();
                 currentChart = drawPublishTimeByCountChart(ctx);
                 break;
             case "Categories by Average Views":
                 currentChart.destroy();
+                $("#wordcloud").empty();
                 currentChart = drawCategoryByViewsChart(ctx);
                 break;
             case "Likes, Dislikes, and Comments":
                 currentChart.destroy();
+                $("#wordcloud").empty();
                 currentChart = drawDonutChart(ctx);
+                break;
+            case "Video Tags Word Cloud":
+                currentChart.destroy();
+                $("#wordcloud").empty();
+                drawWordCloud();
                 break;
             default:
                 console.log("Default");
                 currentChart.destroy();
                 currentChart = drawPublishTimeByViewsChart(ctx);
                 break;
-        }
-    });
-
-    // Updates the current country text when changed
-    $("#dropdown1 li").click(function() {
-        var text = $(this).text();
-        $("#current_country").html(text);
-        if (text != "USA") {
-            $("#note").show();
-        } else {
-            $("#note").hide();
         }
     });
 });
